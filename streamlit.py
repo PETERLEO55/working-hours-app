a69259d08b3a4b83c0252687a72579facd6cbabe
PETERLEO55 <101087828+PETERLEO55@users.noreply.github.com>
2026-03-03 11:59:53 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -18,14 +18,15 @@ if st.button('Calculate'):
     now = datetime.now(ist)
     total_sec = 0
     for i,j in zip(start, end):
-        t1 = datetime.combine(now.date(),datetime.strptime(i, '%I:%M:%S %p').time())
-        t2 = datetime.combine(now.date(),datetime.strptime(j, '%I:%M:%S %p').time())
+        t1 = datetime.combine(now.date(),datetime.strptime(i, '%I:%M:%S %p').time(),ist)
+        t2 = datetime.combine(now.date(),datetime.strptime(j, '%I:%M:%S %p').time(),ist)
         total_sec += (t2-t1).total_seconds()
     if len(start) > len(end):
-        last_in = datetime.combine(now.date(),datetime.strptime(start[-1], '%I:%M:%S %p').time())
+        last_in = datetime.combine(now.date(),datetime.strptime(start[-1], '%I:%M:%S %p').time(),ist)
         total_sec += (now - last_in).total_seconds()
     st.success(f"Total working hours: {total_sec // 3600} hours {(total_sec % 3600) // 60} minutes")
     remaining = (((8*3600)+(30*60))-total_sec)
     st.info(f'{remaining//3600} hours left {(remaining%3600)//60} minutes left')
 
     # st.info(f'{remaining//60} hours left') if remaining > 0 else st.info(f'{remaining} minutes left')
+
