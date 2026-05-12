3fe8fd3e4ed0844ca0d13401b28ddcdadd81d047
PeterLeo55 <101087828+PETERLEO55@users.noreply.github.com>
2026-05-12 14:03:33 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,32 +1,40 @@
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 import streamlit as st
 
-st.title('Time tracker')
-
-time = st.text_area("Copy paste the Today's Attendance logs here", height=200)
 ist = pytz.timezone('Asia/Kolkata')
-if st.button('Calculate'):
+
+def working_hours(input_):
     start = []
     end = []
-    for i in time.strip().split('\n'):
+    for i in input_.strip().split('\n'):
         if 'In' in i:
             start.append(i.replace('In','').strip())
         if 'Out' in i:
             end.append(i.replace('Out','').strip())
 
-    now = datetime.now(ist)
+    time_now = datetime.now(ist)
     total_sec = 0
     for i,j in zip(start, end):
-        t1 = datetime.combine(now.date(),datetime.strptime(i, '%I:%M:%S %p').time(),ist)
-        t2 = datetime.combine(now.date(),datetime.strptime(j, '%I:%M:%S %p').time(),ist)
+        t1 = datetime.combine(time_now.date(),datetime.strptime(i, '%I:%M:%S %p').time())
+        t2 = datetime.combine(time_now.date(),datetime.strptime(j, '%I:%M:%S %p').time())
         total_sec += (t2-t1).total_seconds()
     if len(start) > len(end):
-        last_in = datetime.combine(now.date(),datetime.strptime(start[-1], '%I:%M:%S %p').time(),ist)
-        total_sec += (now - last_in).total_seconds()
-    st.success(f"Total working hours: {total_sec // 3600} hours {(total_sec % 3600) // 60} minutes")
+        last_in = datetime.combine(time_now.date(),datetime.strptime(start[-1], '%I:%M:%S %p').time())
+        total_sec += (time_now - last_in).total_seconds()
+    st.info(f"Total working hours: {total_sec // 3600} hours {(total_sec % 3600) // 60} minutes")
     remaining = (((8*3600)+(30*60))-total_sec)
-    st.info(f'{remaining//3600} hours left {(remaining%3600)//60} minutes left')
+    st.info(f'Remaining: {remaining//3600} hours left {(remaining%3600)//60} minutes left')
+    r_hours,r_minutes = remaining // 3600,(remaining % 3600) // 60
+    remaining = timedelta(hours=r_hours, minutes=r_minutes)
+    now = datetime.now()
+    end_time = now + remaining
+    st.info("End time:", end_time.strftime('%I:%M:%S %p'))
 
-    # st.info(f'{remaining//60} hours left') if remaining > 0 else st.info(f'{remaining} minutes left')
 
+st.title('Time tracker')
+
+time = st.text_area("Copy paste the Today's Attendance logs here", height=200)
+
+if st.button('Calculate'):
+    working_hours(time)
